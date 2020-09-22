11d73a1d4a3640f47694c94455337edd7ecdc128
eemeely <70111944+eemeely@users.noreply.github.com>
2020-09-22 15:57:51 +0200

    Add files via upload

---
+++ polybar/space/launch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar top &
+polybar bottom;
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload top & bottom
+done
